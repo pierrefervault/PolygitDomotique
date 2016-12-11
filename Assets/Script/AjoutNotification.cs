7fa4b91e6b13057ced508d2b9352353b860bb32a
Pierre <pierre.fervault@laposte.net>
2016-12-11 00:40:46 -0500

    Avancement ajout interface

---
+++ Assets/Script/AjoutNotification.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AjoutNotification : MonoBehaviour {
+
+    public Image[] images;
+    public Text[] texts;
+    public Notification[] notifications;
+
+	// Use this for initialization
+	void Start () {
+	    foreach(Notification n in notifications)
+        {
+            addNotif(n);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void addNotif(Notification n)
+    {
+        for (int i = 0; i < this.images.Length; i++)
+        {
+            if (images[i].sprite == null)
+            {
+                images[i].sprite = n.image;
+                texts[i].text = n.texte;
+                i = this.images.Length;
+            }
+        }
+    }
+}
---
+++ Assets/Script/AjoutTache.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AjoutTache : MonoBehaviour {
+
+    public Text[] nomTaches;
+    public Text[] statuts;
+    public Text[] recurrences;
+    public Tache[] taches;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Tache t in taches)
+        {
+            addTache(t);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void addTache(Tache t)
+    {
+        for (int i = 0; i < this.nomTaches.Length; i++)
+        {
+            if (nomTaches[i].text.Equals(""))
+            {
+                nomTaches[i].text = t.nomTache;
+                statuts[i].text = t.statut;
+                recurrences[i].text = t.recurrence;
+                i = this.nomTaches.Length;
+            }
+        }
+    }
+}

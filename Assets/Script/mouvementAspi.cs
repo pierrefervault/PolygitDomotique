a380b227cc8a9253d4fb252aac5ea85d19e094a7
Ethios <cyril.richard95@gmail.com>
2016-12-11 01:20:30 -0500

    ajout d'un robot aspirateur idiot

---
+++ Assets/Script/mouvementAspi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouvementAspi : MonoBehaviour {
+
+    public Vector3 dest;
+    public float minX;
+    public float minZ;
+    public float maxX;
+    public float maxZ;
+
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponentInParent<NavMeshAgent>();
+        randDest();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (agent.remainingDistance < 0.5)
+        {
+            randDest();
+        }
+	}
+
+    void randDest()
+    {
+        float randX = Random.RandomRange(minX, maxX);
+        float randZ = Random.RandomRange(minZ, maxZ);
+        dest = new Vector3(randX, transform.position.y, randZ);
+        agent.SetDestination(dest);
+    }
+}

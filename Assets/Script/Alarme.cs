f93077e958802a8851911aac91feb8a98579db33
Ethios <cyril.richard95@gmail.com>
2016-12-13 10:40:10 -0500

    ajout de la possibilité de désactiver les alarmes

---
+++ Assets/Script/Alarme.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alarme : MonoBehaviour {
+
+    public string nom;
+    public Color onColor;
+    public Color offColor;
+
+    private Light lumiere;
+
+	// Use this for initialization
+	void Start () {
+        lumiere = GetComponentInChildren<Light>();
+        lumiere.color = onColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void setOn()
+    {
+        lumiere.color = onColor;
+    }
+
+    public void setOff()
+    {
+        lumiere.color = offColor;
+    }
+}
